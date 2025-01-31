ce7baa31cd95a393d1897911e2942216d9d5f15c
andrea-mestre <andrea.mestre@edu.univ-fcomte.fr>
2025-01-31 11:33:53 +0100

    tp3

---
+++ src/backend.mjs
@@ -0,0 +1,18 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Maison').getFullList({
+            sort: 'created',
+        });
+        data = data.map((house) => {
+            house.img= pb.files.getURL(house, house.image_maison);
+            return house;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
